4800b6e31b287093c33effa04e8a903d25455032
Tammer <tammer1337@gmail.com>
2022-02-23 22:28:29 +0100

    Added CRUD Routes to TODO API

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("Hello Server!");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use("/api/todos", require("./routes/todoRoutes"));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
