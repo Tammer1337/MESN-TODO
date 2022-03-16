d97953bf28f879f4887617a82c68fc9f02f6cff4
Tammer <tammer1337@gmail.com>
2022-03-16 21:39:56 +0100

    Added Axios/CORS as well as Login API Call

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const cors = require("cors")
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+
 connectDB();
 
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,3 +1,28 @@
+<script>
+  import axios from "axios";
+
+  let email, password
+
+  async function loginUser() {
+    try {
+      const response = await axios({
+      method: "POST",
+      url: "http://localhost:5000/api/users/login",
+      data: {
+        email,
+        password
+      }
+    });
+    if(response.status === 201){
+      localStorage.setItem('token', response.data.user.token)
+      window.location.href = '/dashboard'
+    } 
+    } catch (error) {
+      console.log(error);
+    }
+  }
+</script>
+
 <div class="flex justify-center h-screen shadow-inherit bg-gray-100 md:bg-[url('../../static/wave-haikei.svg')] bg-[url('../../static/wave-haikei_mobile.svg')] bg-cover">
     <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div class="bg-slate-100 p-8 rounded-lg shadow-md max-w-md w-full space-y-8">
@@ -7,15 +32,15 @@
             </svg>
             <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
           </div>
-          <form class="mt-8 space-y-6" action="#" method="POST">
+          <form class="mt-8 space-y-6">
             <div class="rounded-md shadow-sm -space-y-px">
               <div>
                 <label for="email-address" class="sr-only">Email address</label>
-                <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-sky-500 focus:border-sky-500 focus:z-10 sm:text-sm" placeholder="Email address">
+                <input id="email-address" bind:value={email} name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-sky-500 focus:border-sky-500 focus:z-10 sm:text-sm" placeholder="Email address">
               </div>
               <div>
                 <label for="password" class="sr-only">Password</label>
-                <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-sky-500 focus:border-sky-500 focus:z-10 sm:text-sm" placeholder="Password">
+                <input id="password" bind:value={password} name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-sky-500 focus:border-sky-500 focus:z-10 sm:text-sm" placeholder="Password">
               </div>
             </div>
       
@@ -26,7 +51,7 @@
             </div>
       
             <div>
-              <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-sky-600 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500">
+              <button type="submit" on:click|preventDefault={loginUser} class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-sky-600 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500">
                 <span class="absolute left-0 inset-y-0 flex items-center pl-3">
                   <!-- Heroicon name: solid/lock-closed -->
                   <svg class="h-5 w-5 text-white-500 group-hover:text-sky-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
