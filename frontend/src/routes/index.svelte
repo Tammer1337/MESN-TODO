d4e390c82fb678622bd8e3eee066f735dcd780e4
Tammer <tammer1337@gmail.com>
2022-03-16 20:58:24 +0100

    Create a register component

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -3,8 +3,8 @@
         <div class="bg-slate-100 p-8 rounded-lg shadow-md max-w-md w-full space-y-8">
           <div>
             <svg xmlns="http://www.w3.org/2000/svg" class="mx-auto h-12 w-auto text-sky-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
-              </svg>
+              <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+            </svg>
             <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
           </div>
           <form class="mt-8 space-y-6" action="#" method="POST">
@@ -21,7 +21,7 @@
       
             <div class="flex items-center justify-end">
               <div class="text-sm">
-                <a href="" class="font-medium text-sky-600 hover:text-sky-500"> No account? Register here!</a>
+                <a href="/register" class="font-medium text-sky-600 hover:text-sky-500"> No account? Register here!</a>
               </div>
             </div>
       
