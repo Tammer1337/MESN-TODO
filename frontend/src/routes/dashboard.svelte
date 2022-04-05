d2f074f6e9eba4808a68d4bbc0a96feb4151dfc4
Tammer <tammer1337@gmail.com>
2022-04-05 21:56:13 +0200

    Deleted Debugging Paragraph in Completed Tasks

--- frontend/src/routes/dashboard.svelte
+++ frontend/src/routes/dashboard.svelte
@@ -150,7 +150,6 @@
     <h3 class="text-2xl  font-bold pb-2">{done.title}</h3>
     <div class="flex flex-row justify-between align-middle">
       <p class="w-auto">{done.description}</p>
-      <p>{done._id}</p>
       <button on:click={()=>deleteTodo(done._id)}>
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
           <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
